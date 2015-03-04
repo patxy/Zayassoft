b31f549c8fb93dbd681a48685912b5aacf3b3449
aitor <aitor.cubi@gmail.com>
2015-03-04 09:44:20 +0100

    dice hola mundo
    y suma 2 mas 2 y te dice su resultado

--- holamundo/src/holamundo/Holamundo.java
+++ holamundo/src/holamundo/Holamundo.java
@@ -2,8 +2,10 @@ package holamundo;
 
 public class Holamundo {
 	public static void main(String[] args) {
-		int j;
-		int m;
+		int j=2;
+		int m=2;
+		int suma=j+m;
 		System.out.println("hola mundo");	
+		System.out.println(suma);
 	}
 }
