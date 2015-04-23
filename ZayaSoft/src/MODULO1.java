1db687d4429822141d76cf3d9033b209b01fef39
aitor <aitor.cubi@gmail.com>
2015-04-23 09:01:19 +0200

    version 1.0 de zayassoft

---
+++ ZayaSoft/src/MODULO1.java
@@ -0,0 +1,8 @@
+
+public class MODULO1{
+	public String cosa(){
+	String s=new String();
+	s="hola q tal";
+	return s;
+}
+}
---
+++ ZayaSoft/src/ZayaSoft.java
@@ -0,0 +1,11 @@
+
+public class ZayaSoft {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int suma=a+b;
+		System.out.println(suma);
+	}
+
+}
